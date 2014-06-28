4b19008b99216ba92c4a0394de0fbefcf1390846
Bob Lauer <rlauer@gmail.com>
2014-06-28 17:54:22 -0500

    update

--- movies/assets/home-movies.js
+++ movies/assets/home-movies.js
@@ -224,7 +224,6 @@ var define, requireModule, require, requirejs;
 
     MovieModel.reopenClass({
       FIXTURES: [
-        { title: '1986 - Mark & Jennifer\'s Wedding', id: 'eMivZwmfZyk' },
         { title: '1986 - Beth & Bobby singing Rainbow Connection', id: 'mWF2WemYgfw' },
         { title: '1986 - Beth and Joey dancing', id: 'zxaR7KVJzjI' },
         { title: '1986 - Bobby and Dana talking at dinner in WI', id: 'ymUfZIwonj8' },
@@ -236,6 +235,7 @@ var define, requireModule, require, requirejs;
         { title: '1986 - Joey fishing', id: 'x33TFD7Ee-I' },
         { title: '1986 - Joey trying to ski', id: '2pB1Ml43pKg' },
         { title: '1986 - Kids playing in the boat', id: 'peGV3GFAYJU' },
+        { title: '1986 - Mark & Jennifer\'s Wedding', id: 'eMivZwmfZyk' },
         { title: '1986 - Nonna\'s Surprise Birthday Party (what year?)', id: 'PFvVFZOe0m8' },
         { title: '1986 - Paul doing magic', id: 'mMAzjelDFUU' },
         { title: '1986 - Paul helping kids with fishing poles', id: 'E5-ASkwi6b4' },
@@ -278,7 +278,7 @@ var define, requireModule, require, requirejs;
         { title: '1989 - Everyone down by the water', id: 'IPISX5LWEsg' },
         { title: '1989 - Joey trying to ski', id: 'wLSYO_lWrcA' },
         { title: '1989 - Kids fishing', id: '9AG7lmc_0J4' },
-        { title: '1989 - Paul modeling his church apparel', id: 'mFervK-LG7E' },
+        // { title: '1989 - Paul modeling his church apparel', id: 'mFervK-LG7E' },
         { title: '1989 - Paul saying grace in WI', id: 'ZkDb0js3y_g' },
         { title: '1989 - Ride up to WI', id: 'heS4BRHxnoc' },
         { title: '1989 - Sunset in WI', id: 'HrLXsAfVTnI' },
@@ -374,43 +374,41 @@ var define, requireModule, require, requirejs;
     function program1(depth0,data) {
       
       var buffer = '', stack1;
-      data.buffer.push("\n      <div class='group-title'>\n        ");
-      stack1 = helpers._triageMustache.call(depth0, "group.key", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
-      if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n      </div>\n\n      <ul class='group-list'>\n        ");
+      data.buffer.push("\n        <li class='group'>\n          <strong class='group-title'>");
+      data.buffer.push(escapeExpression(helpers.unbound.call(depth0, "group.key", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data})));
+      data.buffer.push("</strong>\n          <ul class='group-list'>\n            ");
       stack1 = helpers.each.call(depth0, {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(2, program2, data),contexts:[],types:[],data:data});
       if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n      </ul>\n    ");
+      data.buffer.push("\n          </ul>\n        </li>\n      ");
       return buffer;
       }
     function program2(depth0,data) {
       
       var buffer = '', stack1, helper, options;
-      data.buffer.push("\n          <li class='group-item' ");
+      data.buffer.push("\n              <li class='group-item' ");
       data.buffer.push(escapeExpression(helpers.action.call(depth0, "hideMenu", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["STRING"],data:data})));
-      data.buffer.push(">\n            ");
+      data.buffer.push(">\n                ");
       stack1 = (helper = helpers['link-to'] || (depth0 && depth0['link-to']),options={hash:{
         'action': ("hideMenu"),
         'tagName': ("span")
       },hashTypes:{'action': "STRING",'tagName': "STRING"},hashContexts:{'action': depth0,'tagName': depth0},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0,depth0],types:["STRING","ID"],data:data},helper ? helper.call(depth0, "movie", "", options) : helperMissing.call(depth0, "link-to", "movie", "", options));
       if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n          </li>\n        ");
+      data.buffer.push("\n              </li>\n            ");
       return buffer;
       }
     function program3(depth0,data) {
       
-      var buffer = '', stack1;
-      data.buffer.push("\n              ");
-      stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
-      if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n            ");
+      var buffer = '';
+      data.buffer.push("\n                  ");
+      data.buffer.push(escapeExpression(helpers.unbound.call(depth0, "name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data})));
+      data.buffer.push("\n                ");
       return buffer;
       }
 
-      data.buffer.push("<div class='slide-container'>\n  <nav>\n    ");
+      data.buffer.push("<div class='slide-container'>\n  <nav>\n    <ul>\n      ");
       stack1 = helpers.each.call(depth0, "groupedContent", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],data:data});
       if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-      data.buffer.push("\n  </nav>\n  <div class='movie-container'>\n    ");
+      data.buffer.push("\n    </ul>\n  </nav>\n  <div class='movie-container'>\n    ");
       stack1 = helpers._triageMustache.call(depth0, "outlet", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
       if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
       data.buffer.push("\n  </div>\n</div>\n");
@@ -574,6 +572,15 @@ var define, requireModule, require, requirejs;
       ok(true, 'home-movies/tests/test-helper.js should pass jshint.'); 
     });
   });
+;define("home-movies/tests/home-movies/views/movies.jshint", 
+  [],
+  function() {
+    "use strict";
+    module('JSHint - home-movies/views');
+    test('home-movies/views/movies.js should pass jshint', function() { 
+      ok(true, 'home-movies/views/movies.js should pass jshint.'); 
+    });
+  });
 ;define("home-movies/tests/test-helper", 
   ["home-movies/tests/helpers/resolver","ember-qunit"],
   function(__dependency1__, __dependency2__) {
@@ -584,6 +591,18 @@ var define, requireModule, require, requirejs;
     setResolver(resolver);
 
     document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
+  });
+;define("home-movies/views/movies", 
+  ["exports"],
+  function(__exports__) {
+    "use strict";
+    var MoviesView = Ember.View.extend({
+      didInsertElement: function() {
+        $('nav').stickySectionHeaders();
+      }
+    });
+
+    __exports__["default"] = MoviesView;
   });
 ;/*!
  * jQuery JavaScript Library v1.11.1
@@ -70386,318 +70405,69 @@ define("ember-inflector/lib/system/string",
   });
 global.DS = requireModule('ember-data/lib/main')['default'];
 }(Ember.lookup));
-;/*
- * Sidr
- * https://github.com/artberri/sidr
+;/*!
+ * Sticky Section Headers
  *
- * Copyright (c) 2013 Alberto Varela
- * Licensed under the MIT license.
+ * Copyright (c) 2011 Florian Plank (http://www.polarblau.com/)
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * USAGE:
+ *
+ * $('#container').stickySectionHeaders({
+ *   stickyClass      : 'sticky',
+ *   headlineSelector : 'strong'
+ * });
+ *
  */
 
-;(function( $ ){
+(function($){
+  $.fn.stickySectionHeaders = function(options) {
 
-  var sidrMoving = false,
-      sidrOpened = false;
-
-  // Private methods
-  var privateMethods = {
-    // Check for valids urls
-    // From : http://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-an-url
-    isUrl: function (str) {
-      var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-      if(!pattern.test(str)) {
-        return false;
-      } else {
-        return true;
-      }
-    },
-    // Loads the content into the menu bar
-    loadContent: function($menu, content) {
-      $menu.html(content);
-    },
-    // Add sidr prefixes
-    addPrefix: function($element) {
-      var elementId = $element.attr('id'),
-          elementClass = $element.attr('class');
-
-      if(typeof elementId === 'string' && '' !== elementId) {
-        $element.attr('id', elementId.replace(/([A-Za-z0-9_.\-]+)/g, 'sidr-id-$1'));
-      }
-      if(typeof elementClass === 'string' && '' !== elementClass && 'sidr-inner' !== elementClass) {
-        $element.attr('class', elementClass.replace(/([A-Za-z0-9_.\-]+)/g, 'sidr-class-$1'));
-      }
-      $element.removeAttr('style');
-    },
-    execute: function(action, name, callback) {
-      // Check arguments
-      if(typeof name === 'function') {
-        callback = name;
-        name = 'sidr';
-      }
-      else if(!name) {
-        name = 'sidr';
-      }
-
-      // Declaring
-      var $menu = $('#' + name),
-          $body = $($menu.data('body')),
-          $html = $('html'),
-          menuWidth = $menu.outerWidth(true),
-          speed = $menu.data('speed'),
-          side = $menu.data('side'),
-          displace = $menu.data('displace'),
-          onOpen = $menu.data('onOpen'),
-          onClose = $menu.data('onClose'),
-          bodyAnimation,
-          menuAnimation,
-          scrollTop,
-          bodyClass = (name === 'sidr' ? 'sidr-open' : 'sidr-open ' + name + '-open');
-
-      // Open Sidr
-      if('open' === action || ('toggle' === action && !$menu.is(':visible'))) {
-        // Check if we can open it
-        if( $menu.is(':visible') || sidrMoving ) {
-          return;
-        }
-
-        // If another menu opened close first
-        if(sidrOpened !== false) {
-          methods.close(sidrOpened, function() {
-            methods.open(name);
-          });
-
-          return;
-        }
-
-        // Lock sidr
-        sidrMoving = true;
-
-        // Left or right?
-        if(side === 'left') {
-          bodyAnimation = {left: menuWidth + 'px'};
-          menuAnimation = {left: '0px'};
-        }
-        else {
-          bodyAnimation = {right: menuWidth + 'px'};
-          menuAnimation = {right: '0px'};
-        }
-
-        // Prepare page if container is body
-        if($body.is('body')){
-          scrollTop = $html.scrollTop();
-          $html.css('overflow-x', 'hidden').scrollTop(scrollTop);
-        }
-
-        // Open menu
-        if(displace){
-          $body.addClass('sidr-animating').css({
-            width: $body.width(),
-            position: 'absolute'
-          }).animate(bodyAnimation, speed, function() {
-            $(this).addClass(bodyClass);
-          });
-        }
-        else {
-          setTimeout(function() {
-            $(this).addClass(bodyClass);
-          }, speed);
-        }
-        $menu.css('display', 'block').animate(menuAnimation, speed, function() {
-          sidrMoving = false;
-          sidrOpened = name;
-          // Callback
-          if(typeof callback === 'function') {
-            callback(name);
-          }
-          $body.removeClass('sidr-animating');
-        });
-
-        // onOpen callback
-        onOpen();
-      }
-      // Close Sidr
-      else {
-        // Check if we can close it
-        if( !$menu.is(':visible') || sidrMoving ) {
-          return;
-        }
-
-        // Lock sidr
-        sidrMoving = true;
-
-        // Right or left menu?
-        if(side === 'left') {
-          bodyAnimation = {left: 0};
-          menuAnimation = {left: '-' + menuWidth + 'px'};
-        }
-        else {
-          bodyAnimation = {right: 0};
-          menuAnimation = {right: '-' + menuWidth + 'px'};
-        }
-
-        // Close menu
-        if($body.is('body')){
-          scrollTop = $html.scrollTop();
-          $html.removeAttr('style').scrollTop(scrollTop);
-        }
-        $body.addClass('sidr-animating').animate(bodyAnimation, speed).removeClass(bodyClass);
-        $menu.animate(menuAnimation, speed, function() {
-          $menu.removeAttr('style').hide();
-          $body.removeAttr('style');
-          $('html').removeAttr('style');
-          sidrMoving = false;
-          sidrOpened = false;
-          // Callback
-          if(typeof callback === 'function') {
-            callback(name);
-          }
-          $body.removeClass('sidr-animating');
-        });
-
-        // onClose callback
-        onClose();
-      }
-    }
-  };
-
-  // Sidr public methods
-  var methods = {
-    open: function(name, callback) {
-      privateMethods.execute('open', name, callback);
-    },
-    close: function(name, callback) {
-      privateMethods.execute('close', name, callback);
-    },
-    toggle: function(name, callback) {
-      privateMethods.execute('toggle', name, callback);
-    },
-    // I made a typo, so I mantain this method to keep backward compatibilty with 1.1.1v and previous
-    toogle: function(name, callback) {
-      privateMethods.execute('toggle', name, callback);
-    }
-  };
-
-  $.sidr = function( method ) {
-
-    if ( methods[method] ) {
-      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
-    }
-    else if ( typeof method === 'function' || typeof method === 'string' || ! method ) {
-      return methods.toggle.apply( this, arguments );
-    }
-    else {
-      $.error( 'Method ' + method + ' does not exist on jQuery.sidr' );
-    }
-
-  };
-
-  $.fn.sidr = function( options ) {
-
-    var settings = $.extend( {
-      name          : 'sidr',         // Name for the 'sidr'
-      speed         : 200,            // Accepts standard jQuery effects speeds (i.e. fast, normal or milliseconds)
-      side          : 'left',         // Accepts 'left' or 'right'
-      source        : null,           // Override the source of the content.
-      renaming      : true,           // The ids and classes will be prepended with a prefix when loading existent content
-      body          : 'body',         // Page container selector,
-      displace: true, // Displace the body content or not
-      onOpen        : function() {},  // Callback when sidr opened
-      onClose       : function() {}   // Callback when sidr closed
+    var settings = $.extend({
+      stickyClass     : 'sticky',
+      headlineSelector: 'strong'
     }, options);
 
-    var name = settings.name,
-        $sideMenu = $('#' + name);
+    return $(this).each(function() {
+      var $this = $(this);
+      $(this).find('ul:first').bind('scroll.sticky', function(e) {
+        $(this).find('> li').each(function() {
+          var $this      = $(this),
+              top        = $this.position().top,
+              height     = $this.outerHeight(true),
+              $head      = $this.find(settings.headlineSelector),
+              headHeight = $head.outerHeight(true);
 
-    // If the side menu do not exist create it
-    if( $sideMenu.length === 0 ) {
-      $sideMenu = $('<div />')
-        .attr('id', name)
-        .appendTo($('body'));
-    }
-
-    // Adding styles and options
-    $sideMenu
-      .addClass('sidr')
-      .addClass(settings.side)
-      .data({
-        speed          : settings.speed,
-        side           : settings.side,
-        body           : settings.body,
-        displace      : settings.displace,
-        onOpen         : settings.onOpen,
-        onClose        : settings.onClose
-      });
-
-    // The menu content
-    if(typeof settings.source === 'function') {
-      var newContent = settings.source(name);
-      privateMethods.loadContent($sideMenu, newContent);
-    }
-    else if(typeof settings.source === 'string' && privateMethods.isUrl(settings.source)) {
-      $.get(settings.source, function(data) {
-        privateMethods.loadContent($sideMenu, data);
-      });
-    }
-    else if(typeof settings.source === 'string') {
-      var htmlContent = '',
-          selectors = settings.source.split(',');
-
-      $.each(selectors, function(index, element) {
-        htmlContent += '<div class="sidr-inner">' + $(element).html() + '</div>';
-      });
-
-      // Renaming ids and classes
-      if(settings.renaming) {
-        var $htmlContent = $('<div />').html(htmlContent);
-        $htmlContent.find('*').each(function(index, element) {
-          var $element = $(element);
-          privateMethods.addPrefix($element);
+          if (top < 0) {
+            $this.addClass(settings.stickyClass).css('paddingTop', headHeight);
+            $head.css({
+              'top'  : (height + top < headHeight) ? (headHeight - (top + height)) * -1 : '',
+              'width': $this.outerWidth()
+            });
+          } else {
+            $this.removeClass(settings.stickyClass).css('paddingTop', '');
+          }
         });
-        htmlContent = $htmlContent.html();
-      }
-      privateMethods.loadContent($sideMenu, htmlContent);
-    }
-    else if(settings.source !== null) {
-      $.error('Invalid Sidr Source');
-    }
-
-    return this.each(function(){
-      var $this = $(this),
-          data = $this.data('sidr');
-
-      // If the plugin hasn't been initialized yet
-      if ( ! data ) {
-
-        $this.data('sidr', name);
-        if('ontouchstart' in document.documentElement) {
-          $this.bind('touchstart', function(e) {
-            var theEvent = e.originalEvent.touches[0];
-            this.touched = e.timeStamp;
-          });
-          $this.bind('touchend', function(e) {
-            var delta = Math.abs(e.timeStamp - this.touched);
-            if(delta < 200) {
-              e.preventDefault();
-              methods.toggle(name);
-            }
-          });
-        }
-        else {
-          $this.click(function(e) {
-            e.preventDefault();
-            methods.toggle(name);
-          });
-        }
-      }
+      });
     });
   };
 
-})( jQuery );
+  /* A little helper to calculate the sum of different
+   * CSS properties
+   *
+   * EXAMPLE:
+   * $('#my-div').cssSum('paddingLeft', 'paddingRight');
+   */
+  $.fn.cssSum = function() {
+    var $self = $(this), sum = 0;
+    $(arguments).each(function(i, e) {
+      sum += parseInt($self.css(e) || 0, 10);
+    });
+    return sum;
+  };
+
+})(jQuery);
 
 ;define("ic-ajax",
   ["ember","exports"],
